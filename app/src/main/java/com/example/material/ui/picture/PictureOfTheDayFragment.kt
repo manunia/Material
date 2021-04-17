33e5084a253456a2e791e1c37a7a2f3b9460db6d
manunia <timoshenkomaria@mail.ru>
2021-04-17 20:54:14 +0300

    добавлен боттом шит

--- app/src/main/java/com/example/material/ui/picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/material/ui/picture/PictureOfTheDayFragment.kt
@@ -1,20 +1,26 @@
 package com.example.material.ui.picture
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import coil.api.load
 import com.example.material.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.main_fragment.*
 
 class PictureOfTheDayFragment: Fragment() {
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
     private val viewModel: PictureOfTheDayViewModel by lazy {
         ViewModelProviders.of(this).get(PictureOfTheDayViewModel::class.java)
@@ -35,6 +41,14 @@ class PictureOfTheDayFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
+        input_layout.setEndIconOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://en.wikipedia.org/wiki/${input_edit_text.text.toString()}")
+            })
+
+        }
     }
 
 
@@ -69,6 +83,29 @@ class PictureOfTheDayFragment: Fragment() {
             setGravity(Gravity.BOTTOM, 0, 250)
             show()
         }
+    }
+
+    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+        bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                toast("onSlide")
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_DRAGGING -> toast("STATE_DRAGGING")
+                    BottomSheetBehavior.STATE_COLLAPSED -> toast("STATE_COLLAPSED")
+                    BottomSheetBehavior.STATE_EXPANDED -> toast("STATE_EXPANDED")
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> toast("STATE_HALF_EXPANDED")
+                    BottomSheetBehavior.STATE_HIDDEN -> toast("STATE_HIDDEN")
+                    BottomSheetBehavior.STATE_SETTLING -> toast("STATE_SETTLING")
+                }
+
+            }
+        })
     }
 
     companion object {
