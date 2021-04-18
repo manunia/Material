080c6078fe115f3c357ac6a421848398ed97740d
manunia <timoshenkomaria@mail.ru>
2021-04-18 13:59:56 +0300

    добавлен скролл вью и анимация фаб

--- app/src/main/java/com/example/material/ui/picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/material/ui/picture/PictureOfTheDayFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.*
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -13,6 +14,7 @@ import androidx.lifecycle.ViewModelProviders
 import coil.api.load
 import com.example.material.MainActivity
 import com.example.material.R
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.main_fragment.*
 
@@ -125,6 +127,21 @@ class PictureOfTheDayFragment: Fragment() {
         val context = activity as MainActivity
         context.setSupportActionBar(view.findViewById(R.id.bottom_app_bar))
         setHasOptionsMenu(true)
+        bottom_app_bar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+        fab.setOnClickListener {
+            if (isMain) {
+                isMain = false
+                bottom_app_bar.navigationIcon = null
+                fab.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.ic_back_fab))
+                bottom_app_bar.replaceMenu(R.menu.menu_bottom_bar_other_screen)
+            } else {
+                isMain = true
+                bottom_app_bar.navigationIcon =
+                    ContextCompat.getDrawable(context,R.drawable.ic_hamburger_menu_bottom_bar)
+                fab.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.ic_plus_fab))
+                bottom_app_bar.replaceMenu(R.menu.menu_bottom_bar)
+            }
+        }
     }
 
 
