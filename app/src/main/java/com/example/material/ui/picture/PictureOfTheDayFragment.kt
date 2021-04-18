6f628a9c062ae65a13d0b0223f58bb692d02b371
manunia <timoshenkomaria@mail.ru>
2021-04-18 13:29:49 +0300

    добавлено меню

--- app/src/main/java/com/example/material/ui/picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/material/ui/picture/PictureOfTheDayFragment.kt
@@ -3,10 +3,7 @@ package com.example.material.ui.picture
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
@@ -14,6 +11,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import coil.api.load
+import com.example.material.MainActivity
 import com.example.material.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.main_fragment.*
@@ -41,16 +39,14 @@ class PictureOfTheDayFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
         input_layout.setEndIconOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW).apply {
                 data = Uri.parse("https://en.wikipedia.org/wiki/${input_edit_text.text.toString()}")
             })
-
         }
+        setBottomAppBar(view)
     }
-
 
     private fun renderData(data: PictureOfTheDayData) {
         when(data) {
@@ -103,10 +99,35 @@ class PictureOfTheDayFragment: Fragment() {
                     BottomSheetBehavior.STATE_HIDDEN -> toast("STATE_HIDDEN")
                     BottomSheetBehavior.STATE_SETTLING -> toast("STATE_SETTLING")
                 }
-
             }
         })
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.app_bar_fav -> toast("Favourite")
+            R.id.app_bar_search -> toast("Search")
+            android.R.id.home -> {
+                activity?.let {
+                    BottomNavDrawerFragment().show(it.supportFragmentManager,"tag")
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setBottomAppBar(view: View) {
+        val context = activity as MainActivity
+        context.setSupportActionBar(view.findViewById(R.id.bottom_app_bar))
+        setHasOptionsMenu(true)
+    }
+
+
 
     companion object {
         fun newInstance() = PictureOfTheDayFragment()
