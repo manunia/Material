674ab3d7e67c6eb9797ca24fab8e01e4a73bd23a
manunia <timoshenkomaria@mail.ru>
2021-04-25 23:05:43 +0300

    устанавливаем тему в активити

--- app/src/main/java/com/example/material/MainActivity.kt
+++ app/src/main/java/com/example/material/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.material
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.material.ui.chips.SETTINGS_SHARED_PREFERENCE
+import com.example.material.ui.chips.THEME_RES_ID
 import com.example.material.ui.picture.PictureOfTheDayFragment
 
+const val MARS_ID = 1
+const val EARTH_ID = 2
+const val MOON_ID = 3
 
 class MainActivity : AppCompatActivity() {
+
+    private val sharedPreferences by lazy { getSharedPreferences(SETTINGS_SHARED_PREFERENCE, Context.MODE_PRIVATE) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +23,23 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, PictureOfTheDayFragment.newInstance())
                 .commitNow()
         }
+
+        when(themeId()) {
+            MARS_ID -> {
+                R.style.MarsTheme
+                this.setTheme(MARS_ID)
+            }
+            EARTH_ID -> {
+                R.style.EarthTheme
+                this.setTheme(EARTH_ID)
+            }
+            MOON_ID -> {
+                R.style.MoonTheme
+                this.setTheme(MOON_ID)
+            }
+            else -> R.style.Theme_Material
+        }
     }
 
+    fun themeId(): Int = sharedPreferences.getInt(THEME_RES_ID,R.style.MarsTheme)
 }
