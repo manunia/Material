dbf82663f45f884af8135c5887cb7adb7974ae7c
manunia <timoshenkomaria@mail.ru>
2021-04-27 22:30:21 +0300

    lesson4

--- app/src/main/java/com/example/material/ui/picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/material/ui/picture/PictureOfTheDayFragment.kt
@@ -34,7 +34,7 @@ class PictureOfTheDayFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_main_start, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
